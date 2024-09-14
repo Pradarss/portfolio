6442a4b5f29902693a78ed3d12b4532e830a476c
Pradarss <verma0025adarsh@gmail.com>
2024-09-14 15:49:06 +0530

    ..

--- src/components/RightComponents/Home.jsx
+++ src/components/RightComponents/Home.jsx
@@ -15,9 +15,44 @@ function Home(){
 
     return(
         <HomeArea>
-        <div onMouseEnter={HandleHover} onMouseLeave={HandleNotHover} style={{height: "60%", width: "76%",position:"relative"}}>
+        {/* <div onMouseEnter={HandleHover} onMouseLeave={HandleNotHover} style={{height: "60%", width: "76%",position:"relative"}}>
             {Hovered?<div style={{backgroundImage: "url('/assets/Pic-cropped.jpg')" , height: "100%", width: "100%", backgroundSize: "contain", backgroundRepeat:"no-repeat", borderRadius: "50%"}}></div>:<div style={{backgroundImage: "url('/assets/toonpic.jpg')" , height: "100%", width: "100%", backgroundSize: "contain", backgroundRepeat:"no-repeat", borderRadius: "50%"}}></div>}
-        </div>
+        </div> */}
+        <div
+    onMouseEnter={HandleHover}
+    onMouseLeave={HandleNotHover}
+    style={{
+      height: "60%",
+      width: "76%",
+      position: "relative",
+      maxWidth: "400px", // Ensures the div doesn’t get too large on bigger screens
+      maxHeight: "400px", // Limits the height
+    }}
+  >
+    {Hovered ? (
+      <div
+        style={{
+          backgroundImage: "url('/assets/Pic-cropped.jpg')",
+          height: "100%",
+          width: "100%",
+          backgroundSize: "cover", // Ensures the image covers the entire div
+          backgroundPosition: "center", // Centers the image
+          borderRadius: "50%",
+        }}
+      ></div>
+    ) : (
+      <div
+        style={{
+          backgroundImage: "url('/assets/toonpic.jpg')",
+          height: "100%",
+          width: "100%",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          borderRadius: "50%",
+        }}
+      ></div>
+    )}
+  </div>
 
         </HomeArea>
     )
