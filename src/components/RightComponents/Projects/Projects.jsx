c115294ec965000de9a6893411bea8be37aeb951
Pradarss <verma0025adarsh@gmail.com>
2024-11-20 22:38:30 +0530

    ocpms

--- src/components/RightComponents/Projects/Projects.jsx
+++ src/components/RightComponents/Projects/Projects.jsx
@@ -11,6 +11,14 @@ const projects = [
     x:"-100px",
   },
   {
+    image: 'url("https://cdn.pixabay.com/photo/2016/11/23/14/45/software-1853385_960_720.jpg")',
+    title: 'OCPMS',
+    para: 'The Organization-Centric Collaborative Project Management System (OCPMS) is a cutting-edge application tailored for organizations to manage projects, tasks, and teams efficiently. Featuring Trello-like task management, project analytics, chat functionality, and separate workspaces, OCPMS streamlines collaboration and decision-making. It also integrates future-ready AI tools to assist with project content creation and planning, making it an essential tool for modern organizations.',
+    tech: ["ReactJS", "NodeJS", "MongoDB", "ExpressJS"],
+    href: 'https://github.com/SukritiSingh07/ocpms/',
+    x: "100px",
+  },
+  {
     image: 'url("https://clickup.com/blog/wp-content/uploads/2019/01/to-do-list-apps.png")',
     title: 'Trello',
     para: 'Trello is a comprehensive task management application designed specifically for managers and their teams to organize, track, and collaborate on tasks efficiently. The app offers a user-friendly interface where tasks can be categorized into boards and cards, making it easier to visualize the workflow.',
